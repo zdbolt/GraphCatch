0cac3b990a247500dbe5b3af4c4be751f54b9aa5
STUDENTS\zdbolt <zdbolt@ualr.edu>
2018-06-22 15:07:20 -0500

    Added mesh generator to create z=x^2+y^2.  Vertices appear to be drawn correctly, but triangles and normals aren't.  Looks like it's an indexing issue.  Solve that, then next steps are 1.  Use partial derivatives to calculate normals for each vertex as it's being calculated in the first double for loop. 2.  Test lighting and make it first-draft easy to look at.

--- Assets/MeshGenerator.cs
+++ Assets/MeshGenerator.cs
@@ -3,12 +3,56 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class MeshGenerator : MonoBehaviour {
-	// Use this for initialization
+    // Use this for initialization
+    [SerializeField]
+    MeshFilter filter;
+    public float size =1;
 	void Start () {
-	}
+        filter.mesh = GenerateMesh();
+    }
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    Mesh GenerateMesh()
+    {
+
+        Mesh mesh = new Mesh();
+        List<Vector3> vectorList = new List<Vector3>();
+        for (int x = -5; x <= 5; x++)
+            for (int y = -5; y <= 5; y++)
+            {
+                vectorList.Add(new Vector3(x, y, (x * x + y * y)));
+            }
+
+        mesh.SetVertices(vectorList);
+
+        List<int> trianglesList = new List<int>();
+        int columnSize = 11;
+        for (int i = 0; i < 100; i++)
+            {
+                trianglesList.Add(i);
+                trianglesList.Add(i + 1 + columnSize);
+                trianglesList.Add(i + columnSize);
+
+                trianglesList.Add(i);
+                trianglesList.Add(i + 1);
+                trianglesList.Add(i + 1 + columnSize);
+            }
+
+        mesh.SetTriangles(trianglesList, 0);
+
+        List<Vector3> normalsList = new List<Vector3>();
+
+        for (int i = 0; i < 100; i++)
+        {
+            normalsList.Add(Vector3.up);
+        }
+
+         mesh.SetNormals(normalsList);
+
+            return mesh;
+    }
 }
